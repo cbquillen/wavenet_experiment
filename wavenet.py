bf5812e9f30e283c208675e58589774b07cfe267
Carl Quillen <cbquillen@yahoo.com>
2017-05-06 14:55:16 -0400

    Allow internal dimension projections...for fun.

--- wavenet.py
+++ wavenet.py
@@ -12,10 +12,10 @@ from tensorflow.contrib.framework import arg_scope
 from ops import mu_law_decode, mu_law_encode
 
 
-def wavnet_block(padded_x, x, num_outputs, rate, kernel_size, skip_dimension,
-                 histogram_summaries, scope):
+def wavenet_block(padded_x, x, num_outputs, num_outputs2, rate, kernel_size,
+                  skip_dimension, histogram_summaries, scope):
     '''
-    wavnet_block: many important convolution parameters (reuse, kernel_size
+    wavenet_block: many important convolution parameters (reuse, kernel_size
     etc.) come from the arg_scope() and are set by wavenet().
 
     Note that convolutions below end up being causal because the
@@ -23,13 +23,13 @@ def wavnet_block(padded_x, x, num_outputs, rate, kernel_size, skip_dimension,
     drop in the "right" way.
     '''
 
-    conv = layers.conv2d(padded_x, num_outputs=num_outputs, rate=rate,
+    conv = layers.conv2d(padded_x, num_outputs=num_outputs2, rate=rate,
                          kernel_size=kernel_size,
                          activation_fn=tf.nn.tanh,
                          normalizer_params=None,
                          scope=scope + '/conv')
 
-    gate = layers.conv2d(padded_x, num_outputs=num_outputs, rate=rate,
+    gate = layers.conv2d(padded_x, num_outputs=num_outputs2, rate=rate,
                          kernel_size=kernel_size,
                          activation_fn=tf.nn.sigmoid,
                          normalizer_params=None,
@@ -107,7 +107,7 @@ def wavenet(inputs, opts, is_training=True, reuse=False, pad_reuse=False,
         }
 
     # The arg_scope below will apply to all convolutions, including the ones
-    # in wavnet_block().
+    # in wavenet_block().
     with arg_scope([layers.conv2d],
                    reuse=reuse, padding='VALID', **normalizer_params):
 
@@ -130,10 +130,10 @@ def wavenet(inputs, opts, is_training=True, reuse=False, pad_reuse=False,
                     reverse=opts.reverse,
                     scope=block_rate+"/pad"+extra_pad_scope)
 
-                x, skip_connection = wavnet_block(
-                    padded_x, x, opts.num_outputs, rate, opts.kernel_size,
-                    opts.skip_dimension, opts.histogram_summaries,
-                    scope=block_rate)
+                x, skip_connection = wavenet_block(
+                    padded_x, x, opts.num_outputs, opts.num_outputs2,
+                    rate, opts.kernel_size, opts.skip_dimension,
+                    opts.histogram_summaries, scope=block_rate)
 
                 with tf.name_scope(block_rate+"_skip".format(i_block, rate)):
                     skip_connections += skip_connection
